993e795b945c5258607a0bbc139371a443e245ce
VigasocoSDL <vigasocosdl@gmail.com>
2014-11-14 21:38:56 +0100

    Se añaden plugins gráficos para pantalla completa de 32 bits, pantalla completa de 8 bits y ventana de 8 bits

---
+++ LinuxSDLVideoPlugins/LinuxSDLFullScreen32bpp.cpp
@@ -0,0 +1,120 @@
+// LinuxSDLFullScreen32bpp.cpp
+//
+/////////////////////////////////////////////////////////////////////////////
+
+#include "LinuxSDLFullScreen32bpp.h"
+#include "IPalette.h"
+
+#ifdef DEBUG
+#include <stdio.h>
+#define DEBUG_FAIL_FUNC printf("%s\n",__func__);
+#else
+#define DEBUG_FAIL_FUNC 
+#endif
+
+void PrintFlags(Uint32 flags);
+	bool LinuxSDLFullScreen32bpp::init(const VideoInfo *vi, IPalette *pal)  
+	{
+		if ( SDL_Init(SDL_INIT_VIDEO) < 0 ) {
+#ifdef DEBUG
+			fprintf(stderr,
+                	"Couldn't initialize SDL: %s\n", SDL_GetError());
+#endif
+			return false;
+		}
+
+		screen = SDL_SetVideoMode(vi->width, vi->height, 32, SDL_HWSURFACE|SDL_DOUBLEBUF|SDL_FULLSCREEN);
+		if ( screen == NULL ) {
+#ifdef DEBUG
+		        fprintf(stderr, "Couldn't set 640x480x32 video mode: %s\n",
+                        SDL_GetError());
+#endif
+		        return false;
+	    	}
+		PrintFlags(screen->flags);
+
+	        // gets a pointer to the game's palette
+	        _palette = (UINT32 *)pal->getRawPalette();
+
+		_isInitialized = true;
+
+		return true;
+	};
+
+	void LinuxSDLFullScreen32bpp::end()  { LinuxSDLBasicDrawPlugin::end(); };
+
+	// no se porque el linkador se queja sin esto, ?no deberia tomar la virtual de LinuxSDLBasicDrawPlugin ?
+	bool LinuxSDLFullScreen32bpp::isInitialized() const  { return LinuxSDLBasicDrawPlugin::isInitialized(); };
+
+
+	bool LinuxSDLFullScreen32bpp::isFullScreen() const  { DEBUG_FAIL_FUNC };
+
+	// bitmap creation/destruction
+	int LinuxSDLFullScreen32bpp::createBitmap(int width, int height) { DEBUG_FAIL_FUNC };  
+	void LinuxSDLFullScreen32bpp::destroyBitmap(int bitmap) { DEBUG_FAIL_FUNC };  
+
+	// bitmap methods
+	void LinuxSDLFullScreen32bpp::setActiveBitmap(int bitmap) { DEBUG_FAIL_FUNC };  
+	void LinuxSDLFullScreen32bpp::compose(int bitmap, int mode, int attr) { DEBUG_FAIL_FUNC };  
+	void LinuxSDLFullScreen32bpp::getDimensions(int &width, int &height) const   { DEBUG_FAIL_FUNC };
+
+	// clipping methods
+	const Rect *LinuxSDLFullScreen32bpp::getClipArea() const   { DEBUG_FAIL_FUNC };
+	void LinuxSDLFullScreen32bpp::setClipArea(int x, int y, int width, int height) { DEBUG_FAIL_FUNC };  
+	void LinuxSDLFullScreen32bpp::setNoClip() { DEBUG_FAIL_FUNC };  
+
+	// drawing methods
+	void LinuxSDLFullScreen32bpp::render(bool throttle)
+	{
+		// SDL_UpdateRect(screen, 0, 0, 0, 0);
+		SDL_Flip(screen);
+	};
+
+	void LinuxSDLFullScreen32bpp::setPixel(int x, int y, int color)
+	{
+		/* Lock the screen for direct access to the pixels */
+		if ( SDL_MUSTLOCK(screen) ) {
+			if ( SDL_LockSurface(screen) < 0 ) {
+#ifdef DEBUG
+		            fprintf(stderr, "Can't lock screen: %s\n", SDL_GetError());
+#endif
+		            return;
+		        }
+		}
+
+
+		int bpp = screen->format->BytesPerPixel;
+    		/* Here p is the address to the pixel we want to set */
+		Uint8 *p = (Uint8 *)screen->pixels + y * screen->pitch + x * bpp;
+
+		*(Uint32 *)p = _palette[color];
+
+		if ( SDL_MUSTLOCK(screen) ) {
+			SDL_UnlockSurface(screen);
+		}
+	};
+
+	void LinuxSDLFullScreen32bpp::drawLine(int x0, int y0, int x1, int y1, int color) { DEBUG_FAIL_FUNC };  
+	void LinuxSDLFullScreen32bpp::drawRect(Rect *rect, int color) { DEBUG_FAIL_FUNC };  
+	void LinuxSDLFullScreen32bpp::drawRect(int x0, int y0, int width, int height, int color) { DEBUG_FAIL_FUNC };  
+	void LinuxSDLFullScreen32bpp::drawCircle(int x, int y, int radius, int color) { DEBUG_FAIL_FUNC };  
+	void LinuxSDLFullScreen32bpp::drawEllipse(int x, int y, int a, int b, int color) { DEBUG_FAIL_FUNC };  
+
+	void LinuxSDLFullScreen32bpp::fillRect(Rect *rect, int color) { DEBUG_FAIL_FUNC };  
+	void LinuxSDLFullScreen32bpp::fillRect(int x0, int y0, int width, int height, int color) { DEBUG_FAIL_FUNC };  
+	void LinuxSDLFullScreen32bpp::fillCircle(int x, int y, int radius, int color) { DEBUG_FAIL_FUNC };  
+	void LinuxSDLFullScreen32bpp::fillEllipse(int x, int y, int a, int b, int color) { DEBUG_FAIL_FUNC };  
+
+	void LinuxSDLFullScreen32bpp::drawGfx(GfxElement *gfx, int code, int color, int x, int y, int attr) { DEBUG_FAIL_FUNC };  
+	void LinuxSDLFullScreen32bpp::drawGfxClip(GfxElement *gfx, int code, int color, int x, int y, int attr) { DEBUG_FAIL_FUNC };  
+	void LinuxSDLFullScreen32bpp::drawGfxTrans(GfxElement *gfx, int code, int color, int x, int y, int attr, int transData) { DEBUG_FAIL_FUNC };  
+	void LinuxSDLFullScreen32bpp::drawGfxClipTrans(GfxElement *gfx, int code, int color, int x, int y, int attr, int transData) { DEBUG_FAIL_FUNC };  
+
+	// access to custom plugin properties
+	const std::string *LinuxSDLFullScreen32bpp::getProperties(int *num) const  { DEBUG_FAIL_FUNC }; 
+	const int *LinuxSDLFullScreen32bpp::getPropertiesType() const   { DEBUG_FAIL_FUNC };
+	void LinuxSDLFullScreen32bpp::setProperty(std::string prop, int data) { DEBUG_FAIL_FUNC };  
+	void LinuxSDLFullScreen32bpp::setProperty(std::string prop, int index, int data) { DEBUG_FAIL_FUNC };  
+	int LinuxSDLFullScreen32bpp::getProperty(std::string prop) const   { DEBUG_FAIL_FUNC };
+	int LinuxSDLFullScreen32bpp::getProperty(std::string prop, int index) const   { DEBUG_FAIL_FUNC };
+
---
+++ LinuxSDLVideoPlugins/LinuxSDLWindow8bpp.h
@@ -0,0 +1,80 @@
+// LinuxSDLWindow8bpp.h
+//
+//      Class that implements a Linux SDL Windowed 8bpp mode Plugin
+//
+//	Solo se implementa el minimo de funciones necesarias, y no todas
+//	las del interfaz IDrawPlugin
+//
+/////////////////////////////////////////////////////////////////////////////
+
+#ifndef _LINUX_SDL_WINDOW_8BPP_H_
+#define _LINUX_SDL_WINDOW_8BPP_H_
+
+
+#include "IDrawPlugin.h"
+#include "LinuxSDLBasicDrawPlugin.h"
+#include "util/INotificationSuscriber.h"
+
+class LinuxSDLWindow8bpp : public LinuxSDLBasicDrawPlugin, public INotificationSuscriber<IPalette>
+{
+public:
+	LinuxSDLWindow8bpp(){ }
+	 ~LinuxSDLWindow8bpp(){}
+	 bool init(const VideoInfo *vi, IPalette *pal) ;
+	 void end() ;
+
+	// getters
+	 bool isInitialized() const ;
+	 bool isFullScreen() const ;
+
+	// bitmap creation/destruction
+	 int createBitmap(int width, int height) ;
+	 void destroyBitmap(int bitmap) ;
+
+	// bitmap methods
+	 void setActiveBitmap(int bitmap) ;
+	 void compose(int bitmap, int mode, int attr) ;
+	 void getDimensions(int &width, int &height) const ;
+
+	// clipping methods
+	 const Rect *getClipArea() const ;
+	 void setClipArea(int x, int y, int width, int height) ;
+	 void setNoClip() ;
+
+	// drawing methods
+	 void render(bool throttle) ;
+
+	 void setPixel(int x, int y, int color) ;
+
+	 void drawLine(int x0, int y0, int x1, int y1, int color) ;
+	 void drawRect(Rect *rect, int color) ;
+	 void drawRect(int x0, int y0, int width, int height, int color) ;
+	 void drawCircle(int x, int y, int radius, int color) ;
+	 void drawEllipse(int x, int y, int a, int b, int color) ;
+
+	 void fillRect(Rect *rect, int color) ;
+	 void fillRect(int x0, int y0, int width, int height, int color) ;
+	 void fillCircle(int x, int y, int radius, int color) ;
+	 void fillEllipse(int x, int y, int a, int b, int color) ;
+
+	 void drawGfx(GfxElement *gfx, int code, int color, int x, int y, int attr) ;
+	 void drawGfxClip(GfxElement *gfx, int code, int color, int x, int y, int attr) ;
+	 void drawGfxTrans(GfxElement *gfx, int code, int color, int x, int y, int attr, int transData) ;
+	 void drawGfxClipTrans(GfxElement *gfx, int code, int color, int x, int y, int attr, int transData) ;
+
+	// access to custom plugin properties
+	 const std::string *getProperties(int *num) const ;
+	 const int *getPropertiesType() const ;
+	 void setProperty(std::string prop, int data) ;
+	 void setProperty(std::string prop, int index, int data) ;
+	 int getProperty(std::string prop) const ;
+	 int getProperty(std::string prop, int index) const ;
+protected:
+	// palette changed notification
+	virtual void update(IPalette *palette, int data);
+	void updateFullPalette(IPalette *palette);
+//666 prueba...
+private: IPalette *_pal;
+};
+
+#endif // _LINUX_SDL_WINDOW_16BPP_H_
