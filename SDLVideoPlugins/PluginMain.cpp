a534b11bf3909a6c88a121d2e9eb13155c586973
VigasocoSDL <vigasocosdl@gmail.com>
2014-11-17 12:48:29 +0100

    Se añaden plugins gráficos para pantalla completa y ventana en 32, 24, 16 y 8 bpp Se añade un plugin gráfico de 8 bits con paleta de grises. Se renombra carpeta y clases de LinuxSDLVideoPlugins a SDLVideoPlugins, ya que los plugins gráficos valen para cualquier sistema SDL y no solo para Línux Refactorización plugins gráfico SDL para tener menos clases y generar la parte común de casi todos los modos con "templates"

--- SDLVideoPlugins/PluginMain.cpp
+++ SDLVideoPlugins/PluginMain.cpp
@@ -2,14 +2,15 @@
 //
 /////////////////////////////////////////////////////////////////////////////
 
-#include "LinuxSDLWindow32bpp.h"
+#include "SDLVideoPlugins.h"
 
 #include "SDL.h"
 
-static const char *description = "VIGASOCO Linux SDL Plugins v1.0";
+static const char *description = "VIGASOCO Linux SDL Plugins v1.1";
 
 static const char *plugins[] = {
-	 "win32" ,
+	 "win8"  , "win16" , "win24"  , "win32"  , "wingris8" ,
+	 "full8" , "full16", "full24" , "full32" , "fullgris8"
 };
 
 /////////////////////////////////////////////////////////////////////////////
@@ -20,7 +21,25 @@ extern "C" DECLSPEC
 void createPlugin(const char *name,void**a)
 {
 	if (strcmp(name, plugins[0]) == 0){
-		*a=new LinuxSDLWindow32bpp();
+		*a=new SDLDrawPluginWindow8bpp(); 
+	} else if (strcmp(name, plugins[1]) == 0){
+		*a=new SDLDrawPluginWindow16bpp();
+	} else if (strcmp(name, plugins[2]) == 0){
+		*a=new SDLDrawPluginWindow24bpp();
+	} else if (strcmp(name, plugins[3]) == 0){
+		*a=new SDLDrawPluginWindow32bpp();
+	} else if (strcmp(name, plugins[4]) == 0){
+		*a=new SDLDrawPluginWindowPaletaGrises8bpp();
+	} else if (strcmp(name, plugins[5]) == 0){
+		*a=new SDLDrawPluginFullScreen8bpp();
+	} else if (strcmp(name, plugins[6]) == 0){
+		*a=new SDLDrawPluginFullScreen16bpp();
+	} else if (strcmp(name, plugins[7]) == 0){
+		*a=new SDLDrawPluginFullScreen24bpp();
+	} else if (strcmp(name, plugins[8]) == 0){
+		*a=new SDLDrawPluginFullScreen32bpp();
+	} else if (strcmp(name, plugins[9]) == 0){
+		*a=new SDLDrawPluginFullScreenPaletaGrises8bpp();
 	} else {
 		*a=NULL;
 	}
---
+++ SDLVideoPlugins/SDLBasicDrawPlugin.h
@@ -0,0 +1,90 @@
+// SDLBasicDrawPlugin.h
+//
+//      Template with basic drawing functionality
+//
+/////////////////////////////////////////////////////////////////////////////
+
+#ifndef _SDL_BASIC_DRAW_PLUGIN_H_
+#define _SDL_BASIC_DRAW_PLUGIN_H_
+
+#include "SDLDrawPlugin.h"
+
+class DEFAULT
+{
+	public:
+		static const Uint32 flags;
+		static const int bpp;
+};
+
+template<typename T>
+class SDLBasicDrawPlugin : public SDLDrawPlugin
+{
+protected:
+        SDL_Surface *screen;
+	SDL_Surface *surface;
+	bool _isInitialized;
+	UINT32 _flags;
+	int _bpp;
+	T *_palette;
+private:
+	IPalette *_originalPalette;
+public:
+	SDLBasicDrawPlugin(){ screen = NULL; surface = NULL; _isInitialized=false; _flags = DEFAULT::flags; _bpp = DEFAULT::bpp ; _palette = NULL; _originalPalette=NULL; }
+	virtual ~SDLBasicDrawPlugin(){}
+	virtual bool init(const VideoInfo *vi, IPalette *pal);
+	virtual void end(void);
+	// getters
+	 virtual bool isInitialized() const { return _isInitialized; };
+	 virtual bool isFullScreen() const  {};
+
+	// bitmap creation/destruction
+	 virtual int createBitmap(int width, int height)  {};
+	 virtual void destroyBitmap(int bitmap)  {};
+
+	// bitmap methods
+	 virtual void setActiveBitmap(int bitmap) {};
+	 virtual void compose(int bitmap, int mode, int attr) {};
+	 virtual void getDimensions(int &width, int &height) const {};
+
+	// clipping methods
+	 virtual const Rect *getClipArea() const {};
+	 virtual void setClipArea(int x, int y, int width, int height) {};
+	 virtual void setNoClip() {};
+
+	// drawing methods
+	 virtual void render(bool throttle);
+	virtual void setPixel(int x, int y, int color);
+
+	 virtual void drawLine(int x0, int y0, int x1, int y1, int color) {};
+	 virtual void drawRect(Rect *rect, int color) {};
+	 virtual void drawRect(int x0, int y0, int width, int height, int color) {};
+	 virtual void drawCircle(int x, int y, int radius, int color) {};
+	 virtual void drawEllipse(int x, int y, int a, int b, int color) {};
+
+	 virtual void fillRect(Rect *rect, int color) {};
+	 virtual void fillRect(int x0, int y0, int width, int height, int color) {};
+	 virtual void fillCircle(int x, int y, int radius, int color) {};
+	 virtual void fillEllipse(int x, int y, int a, int b, int color) {};
+
+	 virtual void drawGfx(GfxElement *gfx, int code, int color, int x, int y, int attr) {};
+	 virtual void drawGfxClip(GfxElement *gfx, int code, int color, int x, int y, int attr) {};
+	 virtual void drawGfxTrans(GfxElement *gfx, int code, int color, int x, int y, int attr, int transData) {};
+	 virtual void drawGfxClipTrans(GfxElement *gfx, int code, int color, int x, int y, int attr, int transData) {};
+
+	// access to custom plugin properties
+	 virtual const std::string *getProperties(int *num) const {};
+	 virtual const int *getPropertiesType() const {};
+	 virtual void setProperty(std::string prop, int data) {};
+	 virtual void setProperty(std::string prop, int index, int data) {};
+	 virtual int getProperty(std::string prop) const {};
+	 virtual int getProperty(std::string prop, int index) const {};
+
+protected:
+	// palette changed notification
+	virtual void update(IPalette *palette, int data);
+	virtual void updateFullPalette(IPalette *palette);
+};
+
+#include "SDLBasicDrawPluginTemplates.cpp"
+
+#endif // _SDL_BASIC_DRAW_PLUGIN_H_
